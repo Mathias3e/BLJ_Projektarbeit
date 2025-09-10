10df0b35faec7d3468d8be8b1ba352448aceecc4
Mathias3e <mathias.staedelin@ckw.ch>
2025-09-10 11:19:47 +0200

    updated

--- GitHub Push +.bat
+++ GitHub Push +.bat
@@ -2,14 +2,23 @@
 setlocal enabledelayedexpansion
 
 :: Benutzer nach Commit-Nachricht fragen
-set /p msg=Bitte Commit-Nachricht eingeben (updated): 
+set /p msg=Bitte Commit-Nachricht eingeben (Updated): 
 
 :: Falls nichts eingegeben wurde, Standardwert setzen
 if "%msg%"=="" set msg=updated
 
 :: Git-Befehle ausführen
 git add .
-git commit -m "%msg%"
-git push
+if errorlevel 1 goto error
 
+git commit -m "%msg%"
+if errorlevel 1 goto error
+
+git push
+if errorlevel 1 goto error
+
+:: Wenn alles erfolgreich war → Fenster schließen
+exit
+
+:error
 pause
